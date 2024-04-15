032fae9c1bdf3df2b0b1d95a0b61283aecc4c8fc
Sairaj Patil <112862628+sairajpatil108@users.noreply.github.com>
2024-04-15 12:23:31 +0530

    app link added

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+
+const roles = ["Android Developer", "Cross Platform App Developer", "Cybersecurity Analyst", "Java Developer"];
+let currentRoleIndex = 0;
+
+function changeRole() {
+  document.getElementById('role').style.opacity = '0';
+  setTimeout(() => {
+    currentRoleIndex = (currentRoleIndex + 1) % roles.length;
+    document.getElementById('role').innerText = roles[currentRoleIndex];
+    document.getElementById('role').style.opacity = '1';
+  }, 500); // Wait for 0.5 seconds for the transition to complete
 }
+setInterval(changeRole, 2000); // Change role every 3 seconds
 
 
-
-function toggleDescription(descriptionId) {
-  var description = document.getElementById(descriptionId);
-  description.classList.toggle('show');
-}
